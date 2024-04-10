69081629f672a143936b4a57da702d20b3ec8a17
Neil Kapoor <40589159+nkapoor11@users.noreply.github.com>
2024-04-10 16:52:12 -0400

    updated index.js

--- Education_RAG/server/index.js
+++ Education_RAG/server/index.js
@@ -1,10 +1,17 @@
 const webapp = require('./server');
 
 // (5) define the port
-const port = 3001;
+//const port = 3001;
 
 // start the server and connect to the DB
-webapp.listen(port, async () => {
+/**webapp.listen(port, async () => {
+  console.log(`Server running on port: ${port}`);
+});*/
+
+const port = process.env.PORT || 3001;
+const host = '0.0.0.0';
+
+webapp.listen(port, host, async () => {
   console.log(`Server running on port: ${port}`);
 });
 
